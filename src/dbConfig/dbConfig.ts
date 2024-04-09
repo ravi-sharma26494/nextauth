6d0a47a40d8d3ddfb0bd50ba5f066223cefb2c70
Ravi Sharma <ravi.sharma26494@gmail.com>
2024-04-09 15:22:15 +0530

    Updated database, mailtrap config etc

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI!);
+        await mongoose.connect(process.env.MONGO_URI!);
         const connection = mongoose.connection;
         
         connection.on('connected', () => {
